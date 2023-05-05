a211dd43c760dfcd2ae22683f335411b4e8373cf
Xu Daquan <3321943485@0322cumt.onmicrosoft.com>
2023-05-05 21:24:55 +0800

    Create text4.py

---
+++ text4.py
@@ -0,0 +1,16 @@
+n = 3 # number of repetitions
+
+my_string = "abcd"
+
+my_list = [1,2,3]
+
+print(my_string*n)
+
+# abcdabcdabcd
+
+print(my_list*n)
+
+# [1,2,3,1,2,3,1,2,3]
+
+import streamlit as st
+
